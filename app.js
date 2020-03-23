3ea845042aebadabefb7374662dde6c61b00d446
Lu Tang <lutang@MacBook-Pro.hitronhub.home>
2020-03-23 13:41:32 -0700

    Add environment vars

---
+++ app.js
@@ -0,0 +1,93 @@
+require('dotenv').config()
+
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const ejs = require("ejs");
+app.set('view engine', 'ejs');
+
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+// mongoose.connect('mongodb+srv://admin-lu:0629*Salu@cluster0-igwj0.mongodb.net/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    email: String,
+    password: String
+});
+
+UserSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedField:["password"]})
+
+const User = mongoose.model("User", UserSchema);
+
+
+app.get("/", function(req, res){
+  res.render("home")
+})
+
+app.get("/login", function(req, res){
+  res.render("login")
+})
+
+app.get("/register", function(req, res){
+  res.render("register")
+})
+
+app.post("/register", function(req, res){
+  const newUser = new User({email:req.body.email, password:req.body.password})
+  newUser.save(function(err){
+    if(err) {
+      console.log(err)
+    } else {
+      res.render("secrets")
+    }
+  })
+})
+
+app.post("/login", function(req, res){
+  User.findOne({email:req.body.email}, function(err, founderUser){
+    if (err) {
+      console.log(err)
+    } else {
+      if (founderUser) {
+        if (founderUser.password === req.body.password) {
+          res.render("secrets")
+        }
+      }
+    }
+  })
+})
+
+app.get("/submit", function(req, res){
+  res.render("submit")
+})
+
+app.post("/submit", function(req, res){
+  const secret = req.body.secret
+  console.log(secret)
+  res.send("<h1>Thank you for sharing your secrets</h1>")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
